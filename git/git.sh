bcd1e5e571c2a635374ed94382799f174856cbb8
travis holt <trvslhlt@gmail.com>
2016-12-12 15:14:02 -0500

    Add parse_git_branch

--- git/git.sh
+++ git/git.sh
@@ -5,9 +5,16 @@
 sudo apt-get install -y git bash-completion
 
 if grep -q bash-completion ~/.profile; then
-	echo "FOUND"
+	echo "FOUND bash-completion"
 else
 	cat ./git/bash_completion_addition >> ~/.profile
-	echo "NOT FOUND"
+	echo "ADDED bash-completion"
 fi	
 
+
+if grep -q parse_git_branch ~/.profile; then
+	echo "FOUND parse_git_branch"
+else
+	cat ./git/parse_git_branch >> ~/.profile
+	echo "ADDED parse_git_branch"
+fi
